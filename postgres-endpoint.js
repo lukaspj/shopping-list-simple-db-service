a645a7b49bdc5cf9b87e723b421f55b123688528
Lukas Joergensen <lukaspj@outlook.com>
2017-09-05 06:21:48 -0700

    Initial commit

---
+++ postgres-endpoint.js
@@ -0,0 +1,41 @@
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 3000;
+
+const { Client } = require('pg');
+const client = new Client();
+
+client.connect();
+
+client.query(`CREATE TABLE IF NOT EXISTS items (
+                item_id serial,
+                name text,
+                estprice numeric
+              );
+              CREATE TABLE IF NOT EXISTS lists (
+                list_id serial
+              );
+              CREATE TABLE IF NOT EXISTS list_items (
+                list_id integer,
+                item_id integer,
+                amount numeric,
+                notes text
+              )`);
+
+client.end();
+
+app.route('/items')
+    .get((req, res) =>{
+        client.connect();
+        client.query('SELECT * FROM items')
+            .then((dbRes) => {
+                res.json(dbRes.rows);
+                client.end();
+            });
+    });
+
+
+
+app.listen(port);
+
+console.log(`Started listening on ${port}`);
