60611da9509e856e5f1776ecca272e1221a71bdb
Lukas Joergensen <lukaspj@outlook.com>
2017-09-05 07:24:20 -0700

    Allow any connection to the database endpoint

--- postgres-endpoint.js
+++ postgres-endpoint.js
@@ -5,6 +5,11 @@ const express = require('express'),
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 const { Client } = require('pg');
 const client = new Client();
