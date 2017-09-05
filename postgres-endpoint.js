a1941be8177d72f0a70d392846678e99db030fc1
Lukas Joergensen <lukaspj@outlook.com>
2017-09-05 06:48:09 -0700

    Added items/create endpoint

--- postgres-endpoint.js
+++ postgres-endpoint.js
@@ -1,6 +1,10 @@
-var express = require('express'),
+const express = require('express'),
     app = express(),
     port = process.env.PORT || 3000;
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 const { Client } = require('pg');
 const client = new Client();
@@ -29,6 +33,19 @@ client.query(`CREATE TABLE IF NOT EXISTS items (
                 const client = new Client();
                 client.connect();
                 client.query('SELECT * FROM items')
+                    .then((dbRes) => {
+                        res.json(dbRes.rows);
+                        client.end();
+                    });
+            });
+
+        app.route('/items/create')
+            .post((req, res) => {
+                var name = req.body.name;
+                var estprice = req.body.estprice;
+                const client = new Client();
+                client.connect();
+                client.query(`INSERT INTO items (name, estprice) VALUES ("${name}", ${estprice})`)
                     .then((dbRes) => {
                         res.json(dbRes.rows);
                         client.end();
